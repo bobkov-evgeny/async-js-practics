b79f0c5e44841f09ddbcfa9e5ce47911cc4cd7cf
Evgeny_Bobkov <freeedayy@gmail.com>
2021-06-05 11:41:20 +1000

    second challenge (promises)

--- script.js
+++ script.js
@@ -72,10 +72,21 @@ const getCoutryAndNeighbour = function (country) {
 
 getCoutryAndNeighbour('china');
 ----------------------------------------------------------------------------------------------------------------------------------------------------------
-*/
+
+
+const getPosition = function () {
+   return new Promise(function (resolve, reject) {
+      navigator.geolocation.getCurrentPosition(resolve, reject);
+   });
+};
 
 const whereAmI = function (lat, lng) {
-   fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+   getPosition()
+      .then(pos => {
+         const { latitude: lat, longitude: lng } = pos.coords;
+
+         return fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+      })
       .then(response => {
          if (!response.ok) throw new Error('Data not received');
          return response.json();
@@ -144,6 +155,71 @@ const getCoutryData = function (country) {
          countriesContainer.style.opacity = 1;
       });
 };
-//whereAmI(52.508, 13.381);
+
+whereAmI();
 //whereAmI(19.037, 72.873);
-whereAmI(-33.933, 18.474);
+//whereAmI(-33.933, 18.474);
+*/
+/*
+const lotteryPromise = new Promise(function (resolve, reject) {
+   console.log('Lottery draw is happening');
+
+   setTimeout(function () {
+      if (Math.random() >= 0.5) {
+         resolve('You WIN !');
+      } else {
+         reject(new Error('You lost your money.'));
+      }
+   }, 2000);
+});
+
+lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
+
+// Real wolrd example: promisifying setTimeout
+const wait = function (seconds) {
+   return new Promise(resolve => setTimeout(resolve, seconds * 1000));
+};
+
+wait(2)
+   .then(() => {
+      console.log('I waited for 2 seconds');
+      return wait(1);
+   })
+   .then(() => console.log('I waited for 1 second'));
+
+Promise.resolve('abc').then(res => console.log(res));
+Promise.reject('abc').catch(err => console.error(err));
+*/
+let currentImg;
+
+const wait = function (seconds) {
+   return new Promise(resolve => setTimeout(resolve, seconds * 1000));
+};
+
+const createImage = function (imgPath) {
+   return new Promise(function (resolve, reject) {
+      const img = document.createElement('img');
+      img.src = imgPath;
+      img.addEventListener('load', function () {
+         document.querySelector('.images').append(img);
+         currentImg = img;
+         resolve(img);
+      });
+      img.addEventListener('error', function (error) {
+         reject(new Error(`Something broke (${error})`));
+      });
+   });
+};
+
+createImage('/img/img-1.jpg')
+   .then(() => wait(2))
+   .then(() => (currentImg.style.display = 'none'))
+   .then(() => wait(2))
+   .then(() => createImage('/img/img-2.jpg'))
+   .then(() => wait(2))
+   .then(() => (currentImg.style.display = 'none'))
+   .then(() => wait(2))
+   .then(() => createImage('/img/img-3.jpg'))
+   .then(() => wait(2))
+   .then(() => (currentImg.style.display = 'none'))
+   .catch(err => console.error(`Error OCCURED(${err})`));
